4eebed20e2e266de2f9b79a842a150dc4832b0c7
rogegg <rogegg@correo.ugr.es>
2017-01-17 20:42:14 +0100

    model add CHOICE

--- polls/models.py
+++ polls/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_update = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
